aaf6efd3359f929a89fe558ac0e6e4785a1a3bdf
razikdontcare <gamersia4@gmail.com>
2024-12-16 21:24:55 +0800

    feat: create Verification component and update VerificationPage layout with email verification instructions

--- src/app/verification/page.tsx
+++ src/app/verification/page.tsx
@@ -1,3 +1,22 @@
+import Logo from "@/components/icons/logo";
+import Verification from "@/components/Verification";
+import Link from "next/link";
+
 export default function VerificationPage() {
-  return <></>;
+  return (
+    <>
+      <div className="flex flex-col items-center justify-start h-screen overflow-hidden gap-10 pb-10 w-full mx-auto">
+        <Link
+          href="/"
+          className="flex items-center justify-center shadow-xl w-full py-5"
+        >
+          <Logo className="size-20" />
+          <span className="font-bold text-4xl text-primary">UniBiz</span>
+        </Link>
+        <div className="flex flex-col items-center justify-center w-full flex-1">
+          <Verification />
+        </div>
+      </div>
+    </>
+  );
 }
