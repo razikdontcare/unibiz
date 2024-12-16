b27dbdaec5450f197e3078009c8758fa51fbdfa0
razikdontcare <gamersia4@gmail.com>
2024-12-16 19:26:39 +0800

    feat: integrate RegisterEmailContextProvider in RegisterPage for context management

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,19 +1,22 @@
 import Logo from "@/components/icons/logo";
 import Register from "@/components/Register";
+import { RegisterEmailContextProvider } from "@/utils/context";
 import Link from "next/link";
 
 export default function RegisterPage() {
   return (
     <>
-      <div className="flex flex-col items-center justify-start h-screen overflow-hidden py-10 gap-10 w-full mx-auto">
-        <Link href="/" className="flex items-center justify-center">
-          <Logo className="size-36" />
-          <span className="font-bold text-6xl text-primary">UniBiz</span>
-        </Link>
-        <div className="flex items-center justify-center w-full h-full">
-          <Register />
+      <RegisterEmailContextProvider>
+        <div className="flex flex-col items-center justify-start h-screen overflow-hidden py-10 gap-10 w-full mx-auto">
+          <Link href="/" className="flex items-center justify-center">
+            <Logo className="size-36" />
+            <span className="font-bold text-6xl text-primary">UniBiz</span>
+          </Link>
+          <div className="flex items-center justify-center w-full h-full">
+            <Register />
+          </div>
         </div>
-      </div>
+      </RegisterEmailContextProvider>
     </>
   );
 }
