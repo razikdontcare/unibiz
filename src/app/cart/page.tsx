271f315b4fd065da512e87ccee39a3ead602c341
razikdontcare <gamersia4@gmail.com>
2024-12-31 14:41:21 +0800

    refactor: restore ChatIcon in interactiveNavbar and remove unused references in cart page

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,5 +1,4 @@
 import ArrowLeftIcon from "@/components/icons/arrowLeft";
-import ChatIcon from "@/components/icons/chat";
 import Navbar from "@/components/navbar";
 import { clientConfig, serverConfig } from "@/config";
 import { getTokens } from "next-firebase-auth-edge";
@@ -30,18 +29,7 @@ export default async function Cart() {
           <div>
             <h1 className="text-2xl font-bold">Keranjang Saya</h1>
           </div>
-          <div className="relative">
-            <Link href="/chats">
-              <ChatIcon className="size-8" />
-              <div className="bg-primary absolute p-2 rounded-full -top-2 -right-2">
-                <div className="relative">
-                  <span className="absolute text-xs font-bold -top-2 -left-1">
-                    2
-                  </span>
-                </div>
-              </div>
-            </Link>
-          </div>
+          <div className="relative"></div>
         </div>
         <div className="flex items-center justify-between w-full p-5">
           <span>abc</span>
